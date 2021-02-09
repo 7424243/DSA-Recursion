76cbe7d527b34eebbf021024f1ca2d72e45deaba
Sam Sheets <sam.sheets94@gmail.com>
2021-02-09 13:31:27 -0700

    completed Power Calculator problem

--- recursion.js
+++ recursion.js
@@ -11,3 +11,18 @@ const countingSheep = function(number) {
 }
 
 console.log(countingSheep(3))
+
+//2. Power Calculator
+//Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers)
+// powerCalculator(10,2) should return 100
+// powerCalculator(10,-2) should return exponent should be >= 0
+
+const powerCalculator = function(baseInt, exponentInt) {
+    if(exponentInt < 0) {
+        return 'exponent should be >= 0'
+    }
+    if(exponentInt === 1) {
+        return baseInt
+    }
+    return baseInt * powerCalculator(baseInt, exponentInt - 1)
+}

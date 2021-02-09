debb75a1af78bdb5e17de866c11c31f27cafa4f9
Sam Sheets <sam.sheets94@gmail.com>
2021-02-09 14:08:17 -0700

    completed fibonacci problem

--- recursion.js
+++ recursion.js
@@ -31,9 +31,11 @@ const powerCalculator = function(baseInt, exponentInt) {
 //Write a function that reverses a string. Take a string as input, reverse the string, and return the new string.
 
 const reverseString = function(string) {
+    //base case
     if(string.length === 1) {
         return string
     }
+    //general case
     //use a negative number to slice from the end of a string
     return string.slice(-1) + reverseString(string.slice(0,-1)) 
 }
@@ -42,9 +44,11 @@ const reverseString = function(string) {
 //Calculate the nth triangular number. A triangular number counts the objects that can form an equilateral triangle. The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n. This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
 
 const triangularNumber = function(number) {
+    //base case
     if(number === 0) {
         return 0
     }
+    //general case
     return number + triangularNumber(number - 1)
 }
  
@@ -61,3 +65,21 @@ const stringSplitter = function(string, separator) {
 }
 
 console.log(stringSplitter('02/20/2020', '/'))
+
+//6. Fibonacci
+//Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+
+const fibonacci = function(number) {
+    //base case
+    if(number === 0) {
+        return
+    } else if(number === 1) {
+        return 1
+    } else if(number === 2) {
+        return 1
+    }
+    //general case
+    return fibonacci(number - 2) + fibonacci(number - 1)
+}
+
+console.log(fibonacci(7))

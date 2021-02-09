4173aa000b21c10acb586b997c582a19a97ce512
Sam Sheets <sam.sheets94@gmail.com>
2021-02-09 14:32:55 -0700

    completed Organization Chart problem

--- recursion.js
+++ recursion.js
@@ -124,3 +124,52 @@ const anagram = function(subject) {
 }
 
 console.log(anagram('east'))
+
+//11. Organization Chart
+//Write a recursive function that prints the following organization chart. Your output should be as shown below with proper indentation to show the hierarchy. You may store the org chart in an object and send that as an input to your program.
+const organizationChart = function(organization, boss) {
+    let result = {}
+    organization.filter(item => item.boss === boss).forEach(item => result[item.id] = organizationChart(organization, item.id))
+    return result
+}
+
+const org = [
+    {id: 'Zuckerberg', boss: null},
+    {id: 'Schroepfer', boss: 'Zuckerberg'},
+    {id: 'Schrage', boss: 'Zuckerberg'},
+    {id: 'Sandberg', boss: 'Zuckerberg'},
+    {id: 'Bosworth', boss:'Schroepfer' },
+    {id: 'Zhao', boss:'Schroepfer' },
+    {id: 'Steve', boss:'Bosworth' },
+    {id: 'Kyle', boss:'Bosworth' },
+    {id: 'Andra', boss:'Bosworth' },
+    {id: 'Richie', boss: 'Zhao'},
+    {id: 'Sofia', boss: 'Zhao'},
+    {id: 'Jen', boss: 'Zhao'},
+    {id: 'VanDyck', boss:'Schrage' },
+    {id: 'Swain', boss:'Schrage' },
+    {id: 'Sabrina', boss:'VanDyck' },
+    {id: 'Michelle', boss:'VanDyck' },
+    {id: 'Josh', boss:'VanDyck' },
+    {id: 'Blanch', boss:'Swain' },
+    {id: 'Tom', boss:'Swain' },
+    {id: 'Joe', boss:'Swain' },
+    {id: 'Goler', boss:'Sandberg' },
+    {id: 'Hernandez', boss:'Sandberg' },
+    {id: 'Moissinac', boss:'Sandberg' },
+    {id: 'Kelley', boss:'Sandberg' },
+    {id: 'Eddie', boss:'Goler' },
+    {id: 'Julie', boss:'Goler' },
+    {id: 'Annie', boss:'Goler' },
+    {id: 'Rowi', boss:'Hernandez'},
+    {id: 'Inga', boss:'Hernandez'},
+    {id: 'Morgan', boss:'Hernandez'},
+    {id: 'Amy', boss:'Moissinac'},
+    {id: 'Chuck', boss:'Moissinac'},
+    {id: 'Vinni', boss:'Moissinac'},
+    {id: 'Eric', boss:'Kelley'},
+    {id: 'Ana', boss:'Kelley'},
+    {id: 'Wes', boss:'Kelley'},
+]
+
+console.log(JSON.stringify(organizationChart(org, null)))

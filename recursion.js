9cff2b27ffd3888bd4103a279b1dc0f6d486644c
Sam Sheets <sam.sheets94@gmail.com>
2021-02-09 14:39:51 -0700

    completed binary problem

--- recursion.js
+++ recursion.js
@@ -173,3 +173,20 @@ const org = [
 ]
 
 console.log(JSON.stringify(organizationChart(org, null)))
+
+//12. Binary Representation
+//Write a recursive function that prints out the binary representation of a given number. For example, the program should take 3 as an input and print 11 as output, or 25 as an input and print 11001 as an output. Note that the binary representation of 0 should be 0.
+
+const binary = function(number) {
+    if(number === 0) {
+        return 0
+    }
+    const dividedNumber = Math.floor(number/2)
+    const remainder = number % 2
+    if(dividedNumber === 0) {
+        return remainder
+    }
+    return binary(dividedNumber) + remainder.toString()
+}
+
+console.log(binary(25))
